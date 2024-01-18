3318d0263335ceec12247afb97b46c02bb6e5116
EmpreynCh <emprhaimquiambao03@gmail.com>
2024-01-18 21:54:52 +0800

    vbnet Project Successfully

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,353 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TxtNumber = New TextBox()
+        btn7 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btn4 = New Button()
+        btn5 = New Button()
+        btn6 = New Button()
+        btn1 = New Button()
+        btn2 = New Button()
+        btn3 = New Button()
+        btn0 = New Button()
+        btnPeriod = New Button()
+        btnMultiply = New Button()
+        btnAdd = New Button()
+        Button14 = New Button()
+        btnSubtract = New Button()
+        btnEqual = New Button()
+        BtnClear = New Button()
+        btnErase = New Button()
+        btnUp = New Button()
+        btnMod = New Button()
+        SuspendLayout()
+        ' 
+        ' TxtNumber
+        ' 
+        TxtNumber.BackColor = SystemColors.Info
+        TxtNumber.Font = New Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point)
+        TxtNumber.Location = New Point(12, 12)
+        TxtNumber.Name = "TxtNumber"
+        TxtNumber.Size = New Size(326, 87)
+        TxtNumber.TabIndex = 0
+        TxtNumber.Text = "0"
+        TxtNumber.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' btn7
+        ' 
+        btn7.BackColor = SystemColors.InactiveCaptionText
+        btn7.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btn7.ForeColor = SystemColors.GradientInactiveCaption
+        btn7.Location = New Point(12, 123)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(77, 68)
+        btn7.TabIndex = 1
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = False
+        ' 
+        ' btn8
+        ' 
+        btn8.BackColor = SystemColors.InactiveCaptionText
+        btn8.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btn8.ForeColor = SystemColors.GradientInactiveCaption
+        btn8.Location = New Point(95, 123)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(77, 68)
+        btn8.TabIndex = 1
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = False
+        ' 
+        ' btn9
+        ' 
+        btn9.BackColor = SystemColors.InactiveCaptionText
+        btn9.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btn9.ForeColor = SystemColors.GradientInactiveCaption
+        btn9.Location = New Point(178, 123)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(77, 68)
+        btn9.TabIndex = 1
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = False
+        ' 
+        ' btn4
+        ' 
+        btn4.BackColor = SystemColors.InactiveCaptionText
+        btn4.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btn4.ForeColor = SystemColors.GradientInactiveCaption
+        btn4.Location = New Point(12, 197)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(77, 68)
+        btn4.TabIndex = 1
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = False
+        ' 
+        ' btn5
+        ' 
+        btn5.BackColor = SystemColors.InactiveCaptionText
+        btn5.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btn5.ForeColor = SystemColors.GradientInactiveCaption
+        btn5.Location = New Point(95, 197)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(77, 68)
+        btn5.TabIndex = 1
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = False
+        ' 
+        ' btn6
+        ' 
+        btn6.BackColor = SystemColors.InactiveCaptionText
+        btn6.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btn6.ForeColor = SystemColors.GradientInactiveCaption
+        btn6.Location = New Point(178, 197)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(77, 68)
+        btn6.TabIndex = 1
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = False
+        ' 
+        ' btn1
+        ' 
+        btn1.BackColor = SystemColors.InactiveCaptionText
+        btn1.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btn1.ForeColor = SystemColors.GradientInactiveCaption
+        btn1.Location = New Point(12, 271)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(77, 68)
+        btn1.TabIndex = 1
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = False
+        ' 
+        ' btn2
+        ' 
+        btn2.BackColor = SystemColors.InactiveCaptionText
+        btn2.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btn2.ForeColor = SystemColors.GradientInactiveCaption
+        btn2.Location = New Point(95, 271)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(77, 68)
+        btn2.TabIndex = 1
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = False
+        ' 
+        ' btn3
+        ' 
+        btn3.BackColor = SystemColors.InactiveCaptionText
+        btn3.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btn3.ForeColor = SystemColors.GradientInactiveCaption
+        btn3.Location = New Point(178, 271)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(77, 68)
+        btn3.TabIndex = 1
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = False
+        ' 
+        ' btn0
+        ' 
+        btn0.BackColor = SystemColors.InactiveCaptionText
+        btn0.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btn0.ForeColor = SystemColors.GradientInactiveCaption
+        btn0.Location = New Point(95, 345)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(77, 68)
+        btn0.TabIndex = 1
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = False
+        ' 
+        ' btnPeriod
+        ' 
+        btnPeriod.BackColor = SystemColors.InactiveCaptionText
+        btnPeriod.Font = New Font("Segoe UI", 28.2F, FontStyle.Regular, GraphicsUnit.Point)
+        btnPeriod.ForeColor = SystemColors.GradientInactiveCaption
+        btnPeriod.Location = New Point(178, 345)
+        btnPeriod.Name = "btnPeriod"
+        btnPeriod.Size = New Size(77, 68)
+        btnPeriod.TabIndex = 1
+        btnPeriod.Text = "."
+        btnPeriod.UseVisualStyleBackColor = False
+        ' 
+        ' btnMultiply
+        ' 
+        btnMultiply.BackColor = SystemColors.InactiveCaptionText
+        btnMultiply.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btnMultiply.ForeColor = SystemColors.GradientInactiveCaption
+        btnMultiply.Location = New Point(261, 195)
+        btnMultiply.Name = "btnMultiply"
+        btnMultiply.Size = New Size(77, 68)
+        btnMultiply.TabIndex = 1
+        btnMultiply.Text = "*"
+        btnMultiply.UseVisualStyleBackColor = False
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.BackColor = SystemColors.InactiveCaptionText
+        btnAdd.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btnAdd.ForeColor = SystemColors.GradientInactiveCaption
+        btnAdd.Location = New Point(261, 345)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(77, 68)
+        btnAdd.TabIndex = 1
+        btnAdd.Text = "+" & vbCrLf
+        btnAdd.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = SystemColors.InactiveCaptionText
+        Button14.Font = New Font("Microsoft Sans Serif", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        Button14.ForeColor = SystemColors.GradientInactiveCaption
+        Button14.Location = New Point(261, 121)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(77, 68)
+        Button14.TabIndex = 1
+        Button14.Text = "/"
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' btnSubtract
+        ' 
+        btnSubtract.BackColor = SystemColors.InactiveCaptionText
+        btnSubtract.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point)
+        btnSubtract.ForeColor = SystemColors.GradientInactiveCaption
+        btnSubtract.Location = New Point(261, 271)
+        btnSubtract.Name = "btnSubtract"
+        btnSubtract.Size = New Size(77, 68)
+        btnSubtract.TabIndex = 1
+        btnSubtract.Text = "-"
+        btnSubtract.UseVisualStyleBackColor = False
+        ' 
+        ' btnEqual
+        ' 
+        btnEqual.BackColor = SystemColors.InactiveCaptionText
+        btnEqual.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btnEqual.ForeColor = SystemColors.GradientInactiveCaption
+        btnEqual.Location = New Point(261, 419)
+        btnEqual.Name = "btnEqual"
+        btnEqual.Size = New Size(77, 68)
+        btnEqual.TabIndex = 1
+        btnEqual.Text = "="
+        btnEqual.UseVisualStyleBackColor = False
+        ' 
+        ' BtnClear
+        ' 
+        BtnClear.BackColor = SystemColors.InactiveCaptionText
+        BtnClear.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        BtnClear.ForeColor = SystemColors.GradientInactiveCaption
+        BtnClear.Location = New Point(12, 419)
+        BtnClear.Name = "BtnClear"
+        BtnClear.Size = New Size(77, 68)
+        BtnClear.TabIndex = 1
+        BtnClear.Text = "C"
+        BtnClear.UseVisualStyleBackColor = False
+        ' 
+        ' btnErase
+        ' 
+        btnErase.BackColor = SystemColors.InactiveCaptionText
+        btnErase.Font = New Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btnErase.ForeColor = SystemColors.GradientInactiveCaption
+        btnErase.Location = New Point(95, 419)
+        btnErase.Name = "btnErase"
+        btnErase.Size = New Size(77, 68)
+        btnErase.TabIndex = 1
+        btnErase.Text = "⬅︎"
+        btnErase.UseVisualStyleBackColor = False
+        ' 
+        ' btnUp
+        ' 
+        btnUp.BackColor = SystemColors.InactiveCaptionText
+        btnUp.Font = New Font("Microsoft Sans Serif", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        btnUp.ForeColor = SystemColors.GradientInactiveCaption
+        btnUp.Location = New Point(178, 419)
+        btnUp.Name = "btnUp"
+        btnUp.Size = New Size(77, 68)
+        btnUp.TabIndex = 1
+        btnUp.Text = "^"
+        btnUp.UseVisualStyleBackColor = False
+        ' 
+        ' btnMod
+        ' 
+        btnMod.BackColor = SystemColors.InactiveCaptionText
+        btnMod.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point)
+        btnMod.ForeColor = SystemColors.GradientInactiveCaption
+        btnMod.Location = New Point(12, 345)
+        btnMod.Name = "btnMod"
+        btnMod.Size = New Size(77, 68)
+        btnMod.TabIndex = 1
+        btnMod.Text = "MOD"
+        btnMod.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.MenuHighlight
+        ClientSize = New Size(352, 496)
+        Controls.Add(btnMod)
+        Controls.Add(btnUp)
+        Controls.Add(btnSubtract)
+        Controls.Add(btnErase)
+        Controls.Add(btnPeriod)
+        Controls.Add(Button14)
+        Controls.Add(btn3)
+        Controls.Add(btnAdd)
+        Controls.Add(btnMultiply)
+        Controls.Add(btn6)
+        Controls.Add(btn9)
+        Controls.Add(btnEqual)
+        Controls.Add(BtnClear)
+        Controls.Add(btn0)
+        Controls.Add(btn2)
+        Controls.Add(btn1)
+        Controls.Add(btn5)
+        Controls.Add(btn4)
+        Controls.Add(btn8)
+        Controls.Add(btn7)
+        Controls.Add(TxtNumber)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TxtNumber As TextBox
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnPeriod As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btnEqual As Button
+    Friend WithEvents BtnClear As Button
+    Friend WithEvents btnErase As Button
+    Friend WithEvents btnUp As Button
+    Friend WithEvents btnMod As Button
+End Class
---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,194 @@
+﻿Imports System.Threading
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Public Class Form1
+    Dim op As String
+    Dim num As Double = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TxtNumber.Text = "0"
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        If Val(TxtNumber.Text) = 0 Then
+            TxtNumber.Text = "1"
+        Else
+            TxtNumber.Text = TxtNumber.Text + "1"
+        End If
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        If Val(TxtNumber.Text) = 0 Then
+            TxtNumber.Text = "0"
+        Else
+            TxtNumber.Text = TxtNumber.Text + "0"
+        End If
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        If Val(TxtNumber.Text) = 0 Then
+            TxtNumber.Text = "2"
+        Else
+            TxtNumber.Text = TxtNumber.Text + "2"
+        End If
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        If Val(TxtNumber.Text) = 0 Then
+            TxtNumber.Text = "3"
+        Else
+            TxtNumber.Text = TxtNumber.Text + "3"
+        End If
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        If Val(TxtNumber.Text) = 0 Then
+            TxtNumber.Text = "4"
+        Else
+            TxtNumber.Text = TxtNumber.Text + "4"
+        End If
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        If Val(TxtNumber.Text) = 0 Then
+            TxtNumber.Text = "5"
+        Else
+            TxtNumber.Text = TxtNumber.Text + "5"
+        End If
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        If Val(TxtNumber.Text) = 0 Then
+            TxtNumber.Text = "6"
+        Else
+            TxtNumber.Text = TxtNumber.Text + "6"
+        End If
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        If Val(TxtNumber.Text) = 0 Then
+            TxtNumber.Text = "7"
+        Else
+            TxtNumber.Text = TxtNumber.Text + "7"
+        End If
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        If Val(TxtNumber.Text) = 0 Then
+            TxtNumber.Text = "8"
+        Else
+            TxtNumber.Text = TxtNumber.Text + "8"
+        End If
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        If Val(TxtNumber.Text) = 0 Then
+            TxtNumber.Text = "9"
+        Else
+            TxtNumber.Text = TxtNumber.Text + "9"
+        End If
+    End Sub
+
+    Private Sub btnPeriod_Click(sender As Object, e As EventArgs) Handles btnPeriod.Click
+        If btnPeriod.Enabled = True Then
+            TxtNumber.Text = TxtNumber.Text + "."
+            btnPeriod.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        btnEqual.PerformClick()
+        op = "add"
+        If num = 0 Then
+            num = Val(TxtNumber.Text)
+        Else
+            num = num + Val(TxtNumber.Text)
+
+        End If
+        TxtNumber.Text = "0"
+        btnPeriod.Enabled = True
+    End Sub
+
+    Private Sub btnEqual_Click(sender As Object, e As EventArgs) Handles btnEqual.Click
+        If op = "add" Then
+            TxtNumber.Text = num + Val(TxtNumber.Text)
+        ElseIf op = "sub" Then
+            TxtNumber.Text = num - Val(TxtNumber.Text)
+        ElseIf op = "mul" Then
+            TxtNumber.Text = num * Val(TxtNumber.Text)
+        ElseIf op = "div" Then
+            TxtNumber.Text = num / Val(TxtNumber.Text)
+        ElseIf op = "mod" Then
+            TxtNumber.Text = num Mod Val(TxtNumber.Text)
+        ElseIf op = "exp" Then
+            TxtNumber.Text = num ^ Val(TxtNumber.Text)
+        End If
+        op = ""
+        num = 0
+    End Sub
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        op = "sub"
+        If num = 0 Then
+            num = Val(TxtNumber.Text)
+        Else
+            num = num - Val(TxtNumber.Text)
+
+        End If
+        TxtNumber.Text = "0"
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        btnEqual.PerformClick()
+        op = "mul"
+        If num = 0 Then
+            num = Val(TxtNumber.Text)
+        Else
+            num = num * Val(TxtNumber.Text)
+
+        End If
+        TxtNumber.Text = "0"
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        op = "div"
+        If num = 0 Then
+            num = Val(TxtNumber.Text)
+        Else
+            num = num / Val(TxtNumber.Text)
+        End If
+        TxtNumber.Text = "0"
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles BtnClear.Click
+        TxtNumber.Text = "0"
+        num = 0
+        op = ""
+        btnPeriod.Enabled = True
+    End Sub
+
+    Private Sub btnMod_Click(sender As Object, e As EventArgs) Handles btnMod.Click
+        btnEqual.PerformClick()
+        op = "mod"
+        If num = 0 Then
+            num = Val(TxtNumber.Text)
+        Else
+            num = num Mod (TxtNumber.Text)
+        End If
+        TxtNumber.Text = "0"
+        btnPeriod.Enabled = True
+    End Sub
+
+    Private Sub btnUp_Click(sender As Object, e As EventArgs) Handles btnUp.Click
+        op = "exp"
+        If num = 0 Then
+            num = Val(TxtNumber.Text)
+        Else
+            num = num ^ Val(TxtNumber.Text)
+        End If
+        TxtNumber.Text = "0"
+        btnPeriod.Enabled = True
+    End Sub
+
+    Private Sub btnErase_Click(sender As Object, e As EventArgs) Handles btnErase.Click
+
+    End Sub
+End Class
